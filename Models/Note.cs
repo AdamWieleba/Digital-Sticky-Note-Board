83899972530e8852e5eb1d892fabc1df213954f9
Adam Wieleba <123748594+AdamWieleba@users.noreply.github.com>
2026-08-04 13:53:57 +0200

    Uzupełnienie klasy modelu notatki

---
+++ Models/Note.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Note
+{
+    public Guid Id { get; set; }
+
+    public string Title { get; set; } = "";
+
+    public string Content { get; set; } = "";
+
+    public double X { get; set; }
+
+    public double Y { get; set; }
+
+    public string Color { get; set; } = "#FFF59D";
+}
