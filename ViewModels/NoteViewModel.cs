d1926d70714d1264110a491c55bf37e3250417c9
Adam Wieleba <123748594+AdamWieleba@users.noreply.github.com>
2026-08-07 17:22:50 +0200

    Uzupełnij klasę NoteViewModel

---
+++ ViewModels/NoteViewModel.cs
@@ -0,0 +1,87 @@
+using DigitalStickyNoteBoard.Models;
+using System;
+
+namespace DigitalStickyNoteBoard.ViewModels
+{
+    public class NoteViewModel : BaseViewModel
+    {
+        private readonly Note _note;
+
+        public NoteViewModel(Note note)
+        {
+            _note = note;
+        }
+
+        /// <summary>
+        /// Zwraca model notatki.
+        /// </summary>
+        public Note Model => _note;
+
+        public Guid Id => _note.Id;
+
+        public string Title
+        {
+            get => _note.Title;
+            set
+            {
+                if (_note.Title != value)
+                {
+                    _note.Title = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public string Content
+        {
+            get => _note.Content;
+            set
+            {
+                if (_note.Content != value)
+                {
+                    _note.Content = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public double X
+        {
+            get => _note.X;
+            set
+            {
+                if (_note.X != value)
+                {
+                    _note.X = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public double Y
+        {
+            get => _note.Y;
+            set
+            {
+                if (_note.Y != value)
+                {
+                    _note.Y = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public string Color
+        {
+            get => _note.Color;
+            set
+            {
+                if (_note.Color != value)
+                {
+                    _note.Color = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+    }
+}
