5f2a334b4bcc00c42835235d9f0b0231115fcf20
Adam Wieleba <123748594+AdamWieleba@users.noreply.github.com>
2026-08-21 20:43:27 +0200

    Zaimplementuj przeciąganie notatek

--- Views/MainWindow.xaml.cs
+++ Views/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using DigitalStickyNoteBoard.ViewModels;
@@ -7,6 +8,11 @@ namespace DigitalStickyNoteBoard.Views
 {
     public partial class MainWindow : Window
     {
+        private NoteViewModel? _draggedNote;
+        private Point _dragStartPoint;
+        private double _noteStartX;
+        private double _noteStartY;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -17,15 +23,50 @@ namespace DigitalStickyNoteBoard.Views
         private void Note_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
             if (sender is Border border &&
-                border.DataContext is NoteViewModel note)
+                border.DataContext is NoteViewModel note &&
+                DataContext is MainViewModel viewModel)
             {
-                if (DataContext is MainViewModel viewModel)
-                {
-                    viewModel.SelectNote(note);
-                }
+                viewModel.SelectNote(note);
+
+                _draggedNote = note;
+                _dragStartPoint = e.GetPosition(BoardCanvas);
+                _noteStartX = note.X;
+                _noteStartY = note.Y;
+
+                border.CaptureMouse();
 
                 e.Handled = true;
             }
+        }
+
+        private void BoardCanvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (_draggedNote == null)
+                return;
+
+            if (e.LeftButton != MouseButtonState.Pressed)
+                return;
+
+            Point currentPoint = e.GetPosition(BoardCanvas);
+
+            double deltaX = currentPoint.X - _dragStartPoint.X;
+            double deltaY = currentPoint.Y - _dragStartPoint.Y;
+
+            _draggedNote.X = Math.Max(0, _noteStartX + deltaX);
+            _draggedNote.Y = Math.Max(0, _noteStartY + deltaY);
+        }
+
+        private void BoardCanvas_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            if (_draggedNote == null)
+                return;
+
+            if (Mouse.Captured is UIElement element)
+            {
+                element.ReleaseMouseCapture();
+            }
+
+            _draggedNote = null;
         }
 
         private void Board_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
